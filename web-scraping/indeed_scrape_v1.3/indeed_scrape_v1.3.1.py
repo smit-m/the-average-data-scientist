fc8ab7aae6fe3e9ac8e6c5f943734dc7bd53e308
Calvin Cao <calvincao18@gmail.com>
2019-02-14 15:04:29 -0500

    indeed scrape v131 initial upload

---
+++ web-scraping/indeed_scrape_v1.3/indeed_scrape_v1.3.1.py
@@ -0,0 +1,126 @@
+import os
+import time
+from time import strftime, sleep
+from datetime import datetime, timedelta
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Setup Working Directory
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# Define Global Variables
+chrome_path = '/usr/bin/chromedriver'
+
+# Configure Headless ChromeDriver
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+# Read Search Inputs
+with open('q_jobtitles.txt', 'r', encoding='utf-8') as fh:
+    qt = fh.read().split()
+with open('q_states.txt', 'r', encoding='utf-8') as fh:
+    qs = fh.read().split()
+
+
+# Define basic scrape function to scrape one designation and one state at a time
+def scrape_basic(chrome_driver, q_title, q_state):
+    print('\n' + q_title, q_state)
+    # Initialize Output List
+    output = []
+    # Initialize search url
+    search_url = 'https://www.indeed.com/jobs?q={}&l={}&sort=date'.format(
+        q_title, q_state
+    )
+    # Open up 1st search page
+    chrome_driver.get(search_url)
+    # Loop the scrape 100 times
+    for i in range(1):
+        # pop window detection #
+        try:
+            pop_window = chrome_driver.find_element_by_css_selector(
+                ".popover.popover-foreground.jobalert-popover")
+        except:
+            pop_window = None
+        if pop_window:
+            x_icon = chrome_driver.find_element_by_class_name("popover-x")
+            x_icon.click()
+        # print current page number #
+        try:
+            print(chrome_driver.find_element_by_id('searchCount').text)
+        except:
+            print('Bad page. Scrape stopped.')
+            time.sleep(5)
+            exit()
+        # find all LEGIT jobs on current page #
+        jobs = []
+        for x in chrome_driver.find_elements_by_css_selector(".row.result.clickcard"):
+            try:
+                sponsored = x.find_element_by_class_name(" sponsoredGray ")
+            except:
+                sponsored = None
+            if sponsored:
+                continue
+            else:
+                jobs.append(x)
+            continue
+        # extract useful information from each listing #
+        for job in jobs:
+            # find designation title and page link & clean them up
+            try:  # designation type 1
+                designation1 = job.find_element_by_css_selector(".jobtitle.turnstileLink") \
+                    .text.replace('\t', ' ').replace('\n', ' ').strip()
+                page_link = job.find_element_by_css_selector(".jobtitle.turnstileLink") \
+                    .get_attribute("href")
+            except:  # designation type 2
+                try:
+                    designation1 = job.find_element_by_class_name("turnstileLink") \
+                        .text.replace('\t', ' ').replace('\n', ' ').strip()
+                    page_link = job.find_element_by_class_name("turnstileLink") \
+                        .get_attribute("href")
+                except:  # if designation not found
+                    designation1 = 'NA'
+                    page_link = 'NA'
+            # find company name & clean it up
+            try:
+                comp_name = job.find_element_by_class_name("company").text.replace('\t', ' ') \
+                    .replace('\n', ' ').strip()
+            except:
+                comp_name = 'NA'
+            # find location & clean it up
+            location = job.find_element_by_class_name("location").text.replace('\t', ' ') \
+                .replace('\n', ' ').strip()
+            # gather all information
+            output.append([0, designation1, comp_name, location, page_link,
+                           time.strftime("%Y-%m-%d")])
+            continue
+        # find and press "Next" button #
+        chrome_driver.execute_script("window.scrollTo(0, 100000)")
+        next_b = chrome_driver.find_elements_by_class_name("np")
+        try:
+            if len(next_b) == 2:
+                next_b = next_b[1]
+            else:
+                next_b = next_b[0]
+                if next_b.text == "« Previous":
+                    break
+            next_b.click()
+        except IndexError:
+            print('No more page. Moving on')
+            break
+        # time.sleep(1)
+        continue
+    return output
+
+
+# Define function to execute the scrape for basic info
+def exec_scrape_basic(c_path, c_options, q_titles, q_states):
+    output = []
+    chrome =  webdriver.Chrome(c_path, chrome_options=c_options)
+    for q_title in q_titles:
+        for q_state in q_states:
+            output += scrape_basic(chrome, q_title, q_state)
+    return output
+
+
+b_out = exec_scrape_basic(chrome_path, options, qt, qs)
