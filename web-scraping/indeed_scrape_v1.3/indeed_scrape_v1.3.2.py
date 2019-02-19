c657843a2ae6114f27c28c70a5d63811f6493278
Calvin Cao <calvincao18@gmail.com>
2019-02-19 14:12:52 -0500

    is_v132 update

--- web-scraping/indeed_scrape_v1.3/indeed_scrape_v1.3.2.py
+++ web-scraping/indeed_scrape_v1.3/indeed_scrape_v1.3.2.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import time
 from selenium import webdriver
 from selenium.common import exceptions as sce
@@ -8,6 +7,12 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 # Setup working directory to script's location
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# Setup ChromeDriver (headless)
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+chrome_path = '/usr/bin/chromedriver'
 
 
 # Define the 'close_popup' function to find popup window (if exist) and close it
@@ -30,11 +35,12 @@ def close_popup(driver):
 def load_page(driver, c_url):
     close_popup(driver=driver)
     try:
+        # Detect the opened page is loaded correctly or not
         page_response = driver.find_element_by_id('searchCount').text
     except sce.NoSuchElementException:
         page_response = None
         print('Bad page, try again')
-        # Try to load the page another n times
+        # Try to load the page another 4 times
         tries = 4
         for t in range(1, tries+1):
             driver.get(c_url)
@@ -174,12 +180,6 @@ with open('q_jobtitles.txt', 'r', encoding='utf-8') as fh:
     qt = list(i.replace(' ', '+') for i in fh.read().strip().split('\n'))
 with open('q_states.txt', 'r', encoding='utf-8') as fh:
     qs = fh.read().strip().split('\n')
-
-# Setup ChromeDriver (headless)
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-chrome_path = '/usr/bin/chromedriver'
 
 # Execute basic scrape
 b_out = exec_scrape_basic(chrome_path, options, qt, qs)
